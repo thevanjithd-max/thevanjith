8296aa1d873f8e1c28999970bff92358bbc848c6
thevanjithd-max <thevanjithd@gmail.com>
2026-02-26 16:42:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["Professional CV and Resume Specialist." , "Creative Presentation Designer." , " Microsoft Excel and Data Expert."],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
